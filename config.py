e79be7eb880ff09f9df0a33a37014d85ac97419e
Martineja Coats <97928462+Mcoats19@users.noreply.github.com>
2024-12-05 17:00:55 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration settings
+
+# Path to Gmail API credentials
+GMAIL_CLIENT_SECRET_FILE = 'credentials/credentials.json'
+
+# Gmail API Scopes
+GMAIL_SCOPES = ['https://www.googleapis.com/auth/gmail.send']
